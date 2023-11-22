33d7964d533c96dc38a713b455bd05e25bf2fa55
unfinishedideas <unfinishedideas@gmail.com>
2023-11-22 11:30:42 -0800

    added spin_sleep to fix timing issues

--- src/main.rs
+++ src/main.rs
@@ -20,20 +20,15 @@ fn main() {
 
     let metronome_thread = thread::spawn(move || {
         let mut looping = true;
-        // Get a output stream handle to the default physical sound device
         let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        // Load a sound from a file, using a path relative to Cargo.toml
         loop {
             if looping {
                 let file = BufReader::new(File::open("./src/assets/EmeryBoardClick.wav").unwrap());
-                // Decode that sound file into a source
                 let source = Decoder::new(file).unwrap();
-                // Play the sound directly on the device
                 stream_handle.play_raw(source.convert_samples());
                 let now = time::Instant::now();
-                std::thread::sleep(std::time::Duration::from_millis(500));
+                spin_sleep::sleep(std::time::Duration::from_millis(bpm_clone.load(Ordering::Relaxed)));
                 println!("{:?}", now.elapsed());
-                // std::thread::sleep(std::time::Duration::from_millis(bpm_clone.load(Ordering::Relaxed)));
             }
             looping = metronome_running_clone.load(Ordering::Relaxed);
         }
